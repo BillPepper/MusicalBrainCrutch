5cb1cb47d0183a3586301371b6ea1bd52823a9c6
Mattes Wenzel <mattes@artfacts.net>
2019-04-29 17:41:43 +0200

    moaaw test data

--- app/src/main/java/net/flawlesslogic/musicalbraincrutch/MainActivity.java
+++ app/src/main/java/net/flawlesslogic/musicalbraincrutch/MainActivity.java
@@ -11,9 +11,12 @@ import java.util.ArrayList;
 public class MainActivity extends AppCompatActivity {
 
     ArrayList<MusicEntry> arrSongList = new ArrayList<MusicEntry>();
-    MusicEntry s1 = new MusicEntry("Toe to Toes", "Mastodon", 0, "EADGBE");
-    MusicEntry s2 = new MusicEntry("Chan chan", "Buena Vista Social Club", 5, "EADGBE");
-    MusicEntry s3 = new MusicEntry("Just Jamming", "Gramatik", 2, "EADGBE");
+    MusicEntry s1 = new MusicEntry(1, "Toe to Toes", "Mastodon", 0, "EADGBE");
+    MusicEntry s2 = new MusicEntry(2, "Chan chan", "Buena Vista Social Club", 5, "EADGBE");
+    MusicEntry s3 = new MusicEntry(3, "Just Jamming", "Gramatik", 2, "EADGBE");
+    MusicEntry s4 = new MusicEntry(3, "Nothing Else Matters", "Metallica", 2, "EADGBE");
+    MusicEntry s5 = new MusicEntry(3, "Celito Lindo", "Unknown", 2, "EADGBE");
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
         arrSongList.add(s1);
         arrSongList.add(s2);
         arrSongList.add(s3);
+        arrSongList.add(s4);
+        arrSongList.add(s5);
 
         MusicEntryAdapter musicEntryAdapter = new MusicEntryAdapter(this, arrSongList);
 
